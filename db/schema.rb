874716623f403191c09829a88a06ac168c2f429b
reooooo <70122112+reooooo@users.noreply.github.com>
2020-09-24 01:53:35 +0900

    ユーザー管理機能実装

--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,7 @@ ActiveRecord::Schema.define(version: 2020_09_21_091223) do
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
-    t.string "nicname", null: false
+    t.string "nickname", null: false
     t.string "family_name", null: false
     t.string "last_name", null: false
     t.string "family_name_kana", null: false
