f56cc3944d4998beb25119d9339e71add735298c
reooooo <70122112+reooooo@users.noreply.github.com>
2020-09-25 19:26:23 +0900

    バリデーションへ変数追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,14 +8,17 @@ class User < ApplicationRecord
                        format: {
                          with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
   with_options presence: true do
-    validates :nickname, 
-    validates :birthday, 
-    VALID_NAME_REGEX = /\A[ぁ-んァ-ン一-龥]/
-    VALID_NAME_KANA_REGEX = /\A[ァ-ヶー－]+\z/
-    validates :family_name, format: { with: VALID_NAME_REGEX }
-    validates :last_name, format: { with: VALID_NAME_REGEX }
-    validates :family_name_kana, format: { with: VALID_NAME_KANA_REGEX }
-    validates :last_name_kana, format: { with: VALID_NAME_KANA_REGEX }
+    validates :nickname
+    validates :birthday
   end
-
+  VALID_NAME_REGEX = /\A[ぁ-んァ-ン一-龥]/                      
+  with_options presence: true, format: { with: VALID_NAME_REGEX } do
+    validates :family_name
+    validates :last_name
+  end
+  VALID_NAME_KANA_REGEX = /\A[ァ-ヶー－]+\z/
+  with_options presence: true, format: { with: VALID_NAME_KANA_REGEX } do
+    validates :family_name_kana
+    validates :last_name_kana
+  end
 end
