b204ca5220a507eb286afa43248c761c65b25a2a
reooooo <70122112+reooooo@users.noreply.github.com>
2020-09-25 18:23:22 +0900

    変数追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,23 +4,18 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, presence: true
   validates :password, length: { minimum: 6},
                        format: {
                          with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
-                               
-  validates :family_name, presence: true,
-                          format: {
-                            with: /\A[ぁ-んァ-ン一-龥]/ }
-  validates :last_name, presence: true,
-                        format: {
-                          with: /\A[ぁ-んァ-ン一-龥]/ }
-  validates :family_name_kana, presence: true,
-                               format: {
-                                 with: /\A[ァ-ヶー－]+\z/ }
-  validates :last_name_kana, presence: true,
-                             format: {
-                               with: /\A[ァ-ヶー－]+\z/ }
-  validates :birthday, presence: true
+  with_options presence: true do
+    validates :nickname, 
+    validates :birthday, 
+    VALID_NAME_REGEX = /\A[ぁ-んァ-ン一-龥]/
+    VALID_NAME_KANA_REGEX = /\A[ァ-ヶー－]+\z/
+    validates :family_name, format: { with: VALID_NAME_REGEX }
+    validates :last_name, format: { with: VALID_NAME_REGEX }
+    validates :family_name_kana, format: { with: VALID_NAME_KANA_REGEX }
+    validates :last_name_kana, format: { with: VALID_NAME_KANA_REGEX }
+  end
 
 end
