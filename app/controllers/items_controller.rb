8b8c08f54d85dc27e5725ce51df332d2ddd0de5d
reooooo <70122112+reooooo@users.noreply.github.com>
2020-10-01 18:13:19 +0900

    修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,7 @@ class ItemsController < ApplicationController
   before_action :move_to_index, only: :new
 
   def index
-    @items = Item.order("created_at DESC")
+    # @items = Item.order("created_at DESC")
   end
 
   def new
