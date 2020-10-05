a1386dce0a24f176e85019e4807c749db81ee73e
reooooo <70122112+reooooo@users.noreply.github.com>
2020-10-05 13:11:47 +0900

    編集機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :move_to_index, only: :new
+  before_action :move_to_index, only: [:new, :edit]
 
   def index
     @items = Item.order("created_at DESC")
@@ -23,6 +23,16 @@ class ItemsController < ApplicationController
 
   def show
     @item = Item.find(params[:id])
+  end
+
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    @item.update(item_params)
+    redirect_to item_path(@item.id)
   end
 
   private
